9bcb4b9f439f42cd45fe85dc148d7d3d963cd645
Alex Vo <votanlean@gmail.com>
2022-09-21 00:31:39 +0700

    Test initialize vault

--- program/tests/test.rs
+++ program/tests/test.rs
@@ -1,27 +1,56 @@
-#![cfg(feature = "test-bpf")]
-
-use std::assert_eq;
-
-use {
-    solana_program::{pubkey::Pubkey, system_program},
-    solana_program_test::ProgramTest,
-    solana_sdk::signature::{Keypair, Signer},
+use borsh::BorshDeserialize;
+use solana_program::system_program;
+use solana_program_test::*;
+use solana_sdk::{
+    account::Account,
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+    signature::{Keypair, Signer},
+    transaction::Transaction,
 };
+use std::mem;
+use tokenswap::entrypoint::process_instruction;
 
-#[test]
-fn test_swaptoken() {
+#[tokio::test]
+async fn test_initialize() {
     let program_id = Pubkey::new_unique();
-    let mut program_test = ProgramTest::default();
-    program_test.add_program("swaptoken", program_id, None);
+    let mint = Pubkey::new_unique();
+    let (vault, _bump_seed) =
+        Pubkey::find_program_address(&[b"vault", &mint.to_bytes()], &program_id);
 
-    let auth = Keypair::new();
-    program_test.add_account(
-        auth.pubkey(),
-        solana_sdk::account::Account {
-            lamports: 100_000_000_000,
-            data: vec![],
-            owner: system_program::id(),
-            ..solana_sdk::account::Account::default()
-        },
+    let mut program_test = ProgramTest::new(
+        "tokenswap", // Run the BPF version with `cargo test-bpf`
+        program_id,
+        processor!(process_instruction), // Run the native version with `cargo test`
+    );
+
+    let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
+
+    // vault does not exist
+    assert_eq!(
+        banks_client.get_account(vault).await.expect("get_account"),
+        None,
+    );
+
+    let mut transaction = Transaction::new_with_payer(
+        &[Instruction::new_with_bincode(
+            program_id,
+            &[0], // ignored but makes the instruction unique in the slot
+            vec![
+                AccountMeta::new(payer.pubkey(), true),
+                AccountMeta::new(vault, false),
+                AccountMeta::new(mint, false),
+                AccountMeta::new(system_program::id(), false),
+            ],
+        )],
+        Some(&payer.pubkey()),
+    );
+    transaction.sign(&[&payer], recent_blockhash);
+    banks_client.process_transaction(transaction).await.unwrap();
+
+    // vault created successfully on the system
+    assert_ne!(
+        banks_client.get_account(vault).await.expect("get_account"),
+        None,
     );
 }
